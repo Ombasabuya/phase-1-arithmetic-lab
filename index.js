132807651c83edff7353ecc25ada01b649a25cfe
ombasaabuya <ombasabuya@gmail.com>
2022-11-27 16:42:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2;
+const  num2 = 31;
+let multiply =num1 * num2;
+let random = Math.floor(Math.random()*0)+1;
+let num3 = 5;
+let num4 = 24;
+let mod = 24 % 5;
+console.mod(mod);
+//let  num=(1,2,3,4, 13,20);
+//let  max =Math.max(num);
+//console.log(max);
+//console.log(random) //
+const max= Math.max(12,13,15,18,19,20);
