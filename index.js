6d97a157deceb43f225e8c39cd79b7d8e5f79a0f
ombasaabuya <ombasabuya@gmail.com>
2022-12-17 05:18:06 +0300

    project completed

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+//Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62
+const num1 = 31;
+const num2 =  2;
+const num3 = 24;
+const num4 = 5;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % num4;
+let max =Math.max(1, 6, 8, 20)
 
-// Write your code here
-const num1 = 2;
-const  num2 = 31;
-let multiply =num1 * num2;
-let random = Math.floor(Math.random()*0)+1;
-let num3 = 5;
-let num4 = 24;
-let mod = 24 % 5;
-console.mod(mod);
-//let  num=(1,2,3,4, 13,20);
-//let  max =Math.max(num);
-//console.log(max);
-//console.log(random) //
-const max= Math.max(12,13,15,18,19,20);
+
+
+ 
