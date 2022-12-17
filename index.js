4a94af7a3cf447052d3833e0f5667d1fbfe95013
ombasaabuya <ombasabuya@gmail.com>
2022-12-17 05:20:50 +0300

    project completed

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const num4 = 5;
 let multiply = num1 * num2;
 let random = Math.floor(Math.random() * 10) + 1;
 let mod = num3 % num4;
-let max =Math.max(1, 6, 8, 20)
+let max =Math.max(1, 4, 19, 20)
 
 
 
